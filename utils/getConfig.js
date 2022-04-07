bdeb00901cdeaa78fe2c11cb3d926fb78c6f0eaa
Yuri Azevedo <yuriazevedo11@gmail.com>
2022-04-07 17:14:50 -0300

    ♻️  refactor: improve getConfig utility function

--- utils/getConfig.js
+++ utils/getConfig.js
@@ -2,41 +2,36 @@ const fs = require('fs');
 const path = require('path');
 const util = require('util');
 
-const readPkg = require('read-pkg-up');
+const findUp = require('find-up');
 const homeDir = require('home-dir');
 
 const commitTypes = require('../lib/types.json');
 
-const defaultConfig = {
-  types: commitTypes,
-  symbol: true,
-  skipQuestions: [''],
-  subjectMaxLength: 75,
-};
+const readFile = util.promisify(fs.readFile);
+
+function loadConfig(filename) {
+  return readFile(filename, 'utf8')
+    .then(JSON.parse)
+    .then((obj) => obj && obj.config && obj.config['cz-emoji'])
+    .catch(() => null);
+}
+
+function loadConfigUpwards(filename) {
+  return findUp(filename).then(loadConfig);
+}
 
 async function getConfig() {
-  const loadConfig = (obj) => obj && obj.config && obj.config['cz-emoji'];
-
-  const readFromPkg = async () =>
-    readPkg().then((res) => (res ? loadConfig(res.packageJson) : null));
-
-  const readFromCzrc = (dir) =>
-    util
-      .promisify(fs.readFile)(dir, 'utf8')
-      .then(JSON.parse, () => null)
-      .then(loadConfig);
-
-  const readFromLocalCzrc = () =>
-    readPkg().then((res) =>
-      res && res.path ? readFromCzrc(`${path.dirname(res.path)}/.czrc`) : null,
-    );
-
-  const readFromGlobalCzrc = () => readFromCzrc(homeDir('.czrc'));
+  const defaultConfig = {
+    types: commitTypes,
+    symbol: true,
+    skipQuestions: [''],
+    subjectMaxLength: 75,
+  };
 
   const config =
-    (await readFromPkg()) ||
-    (await readFromLocalCzrc()) ||
-    (await readFromGlobalCzrc());
+    (await loadConfigUpwards('package.json')) ||
+    (await loadConfigUpwards('.czrc')) ||
+    (await loadConfig(path.join(homeDir(), '.czrc')));
 
   return { ...defaultConfig, ...config };
 }
